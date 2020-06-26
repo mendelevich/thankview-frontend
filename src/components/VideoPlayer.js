ba87d904e36a44765eab77519c6891fc793399d1
Eve Mendelevich <ym9382@gmail.com>
2020-06-26 12:20:29 -0400

    VideoPlayer.js displays working video

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,9 +4,14 @@ import data from '../data/walkthroughs.json';
 let videoData = data[0];
 
 const VideoPlayer = () => {
-  console.log(videoData);
-  // return <video></video>;
-  return <div>hi</div>;
+  return (
+    <div>
+      <video width="320" height="240" controls>
+        <source src={videoData.url} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 };
 
 export default VideoPlayer;
