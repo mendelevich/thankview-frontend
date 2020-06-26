4a3186352fde3ab73ccbbc1767c89d4b46176c0e
Eve Mendelevich <ym9382@gmail.com>
2020-06-26 12:55:25 -0400

    map over walkthrough data

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import VideoPlayer from './components/VideoPlayer';
 
 import data from './data/walkthroughs.json';
 import MenuItem from './components/MenuItem';
-let videoData = data[0];
+// let videoData = data[0];
 
 function App() {
+  console.log(data);
+
   return (
     <div className="App">
       <div className="module">
-        <div id="left" class="screen">
+        <div id="left" className="screen">
           <div id="header">
             <h1>ThankView Walkthrough</h1>
             <p>Total: </p>
           </div>
-          <MenuItem
-            title={videoData.title}
-            minutes={videoData.minutes}
-            seconds={videoData.seconds}
-          />
+          {data.map((videoData, i) => (
+            <MenuItem
+              key={i}
+              title={videoData.title}
+              minutes={videoData.minutes}
+              seconds={videoData.seconds}
+            />
+          ))}
         </div>
-        <div id="right" class="screen">
-          <VideoPlayer url={videoData.url} />
+        <div id="right" className="screen">
+          <VideoPlayer url={data[0].url} />
         </div>
       </div>
     </div>
