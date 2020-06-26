bc5852b8488eba6ce37949d60fac4adf8b74b71a
Eve Mendelevich <ym9382@gmail.com>
2020-06-26 12:27:12 -0400

    added two halves of screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 import VideoPlayer from './components/VideoPlayer';
 
+import data from './data/walkthroughs.json';
+let videoData = data[0];
+
 function App() {
   return (
     <div className="App">
-      <VideoPlayer />
+      <div id="left" class="screen">
+        <h1>ThankView walkthrough</h1>
+      </div>
+      <div id="right" class="screen">
+        <VideoPlayer url={videoData.url} />
+      </div>
     </div>
   );
 }
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import data from '../data/walkthroughs.json';
 
-let videoData = data[0];
-
-const VideoPlayer = () => {
+const VideoPlayer = (props) => {
   return (
     <div>
       <video width="320" height="240" controls>
-        <source src={videoData.url} type="video/mp4" />
+        <source src={props.url} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
